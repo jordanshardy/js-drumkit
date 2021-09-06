e43c69ea58f4790b4b6107be3a233a241f34a322
jshardy13 <jordan.s.hardy@gmail.com>
2021-09-06 21:18:33 +1000

    completed

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-window.addEventListener('keydown', function(e){
+function playSound (e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio)return; // stop running the function if no audio is found for key
   audio.currentTime = 0; // rewind to the start
   audio.play();
-  console.log(key)
-});
+  key.classList.add('playing'); //on key add class list of playing to key variable
+}
+
+function removeTransition(e){
+  if(e.propertyName !== 'transform') return; // skip if it is not a transform
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
